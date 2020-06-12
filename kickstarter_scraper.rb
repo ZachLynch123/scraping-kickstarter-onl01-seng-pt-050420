1c814b259bd3db78e63b10a0bdc5597d7364a930
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-12 08:00:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  kickstarter.css(".project")
+  kickstarter.css(".project").text
   binding.pry
 end
 
