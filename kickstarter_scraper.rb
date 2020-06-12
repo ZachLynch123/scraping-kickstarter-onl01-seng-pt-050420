7d3f75c43ece77ebe8d67e03228415eeee50a8a4
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-06-12 08:07:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,7 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project]
   end
+  projects
   # projects: project = kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # project.css("div.project-thumbnail a img").attribute("src").value
